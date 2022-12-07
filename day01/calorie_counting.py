ecfa9040a077f102b3a4ba8c84d411ecd041bba0
Matt Hammond <matthewbh95@gmail.com>
2022-12-07 17:52:02 -0500

    Formatted day 1 solution.

--- day01/calorie_counting.py
+++ day01/calorie_counting.py
@@ -1,4 +1,6 @@
 import heapq
+
+
 def read_input(filename="input.txt"):
     buffer = []
     with open(filename, "r") as file:
@@ -10,17 +12,23 @@ def read_input(filename="input.txt"):
                 buffer.append(int(line))
         yield buffer
 
+
 def sum_n_largest(n):
     return sum(heapq.nlargest(n, (sum(elf) for elf in read_input())))
+
+
 def part1():
     print(sum_n_largest(1))
+
 
 def part2():
     print(sum_n_largest(3))
 
+
 def main():
     part1()
     part2()
+
 
 if __name__ == "__main__":
     main()
