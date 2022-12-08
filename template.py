ce3bd8181e07a2c1fe51b64517b2f8b16801c4cb
Matt Hammond <matthewbh95@gmail.com>
2022-12-07 20:42:16 -0500

    Create solution template

---
+++ template.py
@@ -0,0 +1,19 @@
+def read_input(filename="input.txt"):
+    with open(filename, "r") as file:
+        return file
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
